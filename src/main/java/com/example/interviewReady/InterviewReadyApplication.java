b2e84092c5abee3c74349bc42ec9fbad5e585853
MoamenAbdelrazik <moamen222101297@ksiu.edu.eg>
2026-07-10 11:19:15 +0300

    feat(backend): implement backend logic in InterviewReadyApplication.java

---
+++ src/main/java/com/example/interviewReady/InterviewReadyApplication.java
@@ -0,0 +1,13 @@
+package com.example.interviewReady;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewReadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewReadyApplication.class, args);
+	}
+
+}
